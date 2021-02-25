9e000f0eea16a2e0538e57571c3dc4f7a1a7d810
AzureAD\SivapavaniUppalapati <siuppala@in.ibm.com>
2021-02-25 17:11:20 +0530

    Eureka Server Configuration changes are done

--- convertcurrency/src/main/java/com/example/convertcurrency/client/CurrencyFactorClient.java
+++ convertcurrency/src/main/java/com/example/convertcurrency/client/CurrencyFactorClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name ="CURRENCYFACTOR", url="http://localhost:8080")
+@FeignClient(name ="CURRENCYFACTOR")
 public interface CurrencyFactorClient {
 
 	@RequestMapping(value = "/currecyconversionfactor/get/{code}", produces = {"*/*"}, method = RequestMethod.GET)
