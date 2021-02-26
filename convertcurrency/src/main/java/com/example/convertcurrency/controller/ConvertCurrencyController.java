92e48afb176da97b3ff471014bed4af6d623f64a
AzureAD\SivapavaniUppalapati <siuppala@in.ibm.com>
2021-02-26 11:08:40 +0530

    Circuit breaker pattern is implemented

--- convertcurrency/src/main/java/com/example/convertcurrency/controller/ConvertCurrencyController.java
+++ convertcurrency/src/main/java/com/example/convertcurrency/controller/ConvertCurrencyController.java
@@ -3,6 +3,8 @@ package com.example.convertcurrency.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +22,29 @@ public class ConvertCurrencyController {
 	@Autowired
 	private CurrencyFactorClient currencyFactorClient;
 	
+	@Autowired
+    private CircuitBreakerFactory circuitBreakerFactory;
+	
 	@GetMapping("/currency/{code}/{amount}")
 	public Double getConversionFactor(@PathVariable("code") String code, @PathVariable("amount") String amount) {
 		
-		Double conversionFactor = currencyFactorClient.getConversionFactor(code);
-		Double amounttoConverted = Double.parseDouble(amount);
-		Double convertedAmount = amounttoConverted * conversionFactor;
-		return convertedAmount;
+		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
+	    
+	    
+	    return (circuitBreaker).run(() -> getConvertedAmount(code, amount), 
+	      throwable -> getDefaultConversionFactor());
 	}
 
 
+	public Double getDefaultConversionFactor() {
+		return 0.0;
+	}
+	
+	public Double getConvertedAmount(String code, String amount) {
+		  Double conversionFactor = currencyFactorClient.getConversionFactor(code);
+		  Double amounttoConverted = Double.parseDouble(amount); 
+		  Double convertedAmount= amounttoConverted * conversionFactor; 
+		  return convertedAmount;
+	}		
+	
 }
