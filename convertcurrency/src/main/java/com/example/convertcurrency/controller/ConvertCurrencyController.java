9a0932a32afd2533e28bab5048bf93955a7aa71a
AzureAD\SivapavaniUppalapati <siuppala@in.ibm.com>
2021-03-03 19:29:27 +0530

    Docker and Kubrenate files are committed

--- convertcurrency/src/main/java/com/example/convertcurrency/controller/ConvertCurrencyController.java
+++ convertcurrency/src/main/java/com/example/convertcurrency/controller/ConvertCurrencyController.java
@@ -3,8 +3,6 @@ package com.example.convertcurrency.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.convertcurrency.client.CurrencyFactorClient;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-
 
 @RestController
 @RequestMapping("/convert")
